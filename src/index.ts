8b15206bab0e2f50ec4973cac9611a1e8b029299
Ivan Kondratyev <ivanbright@gmail.com>
2023-03-19 15:38:08 +0300

    Migrating to TS.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as singleChildRule from "./rules/item-has-single-child";
+
+export default {
+  rules: {
+    "form-item-has-single-child": singleChildRule,
+  },
+};
---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+import { ESLintUtils } from "@typescript-eslint/utils";
+
+export const createRule = ESLintUtils.RuleCreator(
+  () => "https://github.com/MostFrumiousBandersnatch/eslint-plugin-antd"
+);
